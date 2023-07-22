b3d383c517e71bdf1a2fa7eda7bd2adfff080860
Novan <l4novan132@gmail.com>
2023-07-22 11:45:10 +0700

    animate circles

--- canvas.js
+++ canvas.js
@@ -4,35 +4,83 @@ canvas.height = window.innerHeight;
 
 var c = canvas.getContext("2d");
 
-c.fillStyle = "rgba(255, 0, 0, .5)";
-c.fillRect(100, 100, 100, 100);
-c.fillStyle = "rgba(0, 255, 0, .5)";
-c.fillRect(200, 300, 100, 100);
-c.fillStyle = "rgba(255, 255, 0, .5)";
-c.fillRect(400, 300, 100, 100);
+// c.fillStyle = "rgba(255, 0, 0, .5)";
+// c.fillRect(100, 100, 100, 100);
+// c.fillStyle = "rgba(0, 255, 0, .5)";
+// c.fillRect(200, 300, 100, 100);
+// c.fillStyle = "rgba(255, 255, 0, .5)";
+// c.fillRect(400, 300, 100, 100);
+// 
+// console.log(canvas);
+// 
+// // line
+// c.beginpath();
+// c.moveto(50, 300);
+// c.lineto(300, 100);
+// c.lineto(400, 300);
+// c.strokestyle = "#fa3fa8";
+// c.stroke();
+// 
+// // circle
+// 
+// for (let i = 0; i < 100; ++i) {
+//     let x = Math.random() * window.innerWidth;
+//     let y = Math.random() * window.innerHeight;
+//     let r, g, b;
+//     r = Math.random() * 255;
+//     g = Math.random() * 255;
+//     b = Math.random() * 255;
+//     c.beginPath();
+//     c.arc(x, y, 30, 0, Math.PI * 2, false);
+//     c.strokeStyle = `rgba(${r}, ${g}, ${b}, 0.5)`;
+//     c.stroke();
+// 
+// }
 
-console.log(canvas);
+function Circle(x, y, dx, dy, radius) {
+    this.x = x;
+    this.y = y;
+    this.dx = dx;
+    this.dy = dy;
+    this.radius = radius;
 
-// line
-c.beginPath();
-c.moveTo(50, 300);
-c.lineTo(300, 100);
-c.lineTo(400, 300);
-c.strokeStyle = "#fa3fa8";
-c.stroke();
+    this.draw = function() {
+        c.beginPath();
+        c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+        c.strokeStyle = "blue";
+        c.stroke();
+        c.fill();
+    }
 
-// circle
+    this.update = function() {
+        if (this.x + this.radius > innerWidth || this.x - this.radius < 0) this.dx = -this.dx;
+        if (this.y + this.radius > innerHeight || this.y - this.radius < 0) this.dy = -this.dy;
 
-for (let i = 0; i < 100; ++i) {
-    let x = Math.random() * window.innerWidth;
-    let y = Math.random() * window.innerHeight;
-    let r, g, b;
-    r = Math.random() * 255;
-    g = Math.random() * 255;
-    b = Math.random() * 255;
-    c.beginPath();
-    c.arc(x, y, 30, 0, Math.PI * 2, false);
-    c.strokeStyle = `rgba(${r}, ${g}, ${b}, 0.5)`;
-    c.stroke();
-
+        this.x += this.dx;
+        this.y += this.dy;
+    }
 }
+
+
+let circles = [];
+
+for (let i = 0; i < 300; ++i) {
+    let radius = 30;
+    let x = Math.random() * (innerWidth - 2 * radius) + radius;
+    let y = Math.random() * (innerHeight - 2 * radius) + radius;
+    let dx = (Math.random() - 0.5);
+    let dy = (Math.random() - 0.5);
+    circles.push(new Circle(x, y, dx, dy, radius));
+}
+
+
+function animate() {
+    requestAnimationFrame(animate);
+    c.clearRect(0, 0, innerWidth, innerHeight);
+    for (let i = 0; i < circles.length; ++i) {
+        circles[i].draw();
+        circles[i].update();
+    }
+}
+
+animate();
